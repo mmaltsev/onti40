1bd27333ecd7bd3ce8e882dd89a03ebe6d156b46
mmaltsev <d.kruz@mail.ru>
2018-06-16 17:47:36 +0200

    added enrichment result example

--- server/blueprints/wheel/wheel.py
+++ server/blueprints/wheel/wheel.py
@@ -23,40 +23,23 @@ def index():
 def get_wheel_data():
     """ """
     log_cmd('Requested wheel data', 'green')
-    #mtx = [
-    #    [0, 1, 1, 1, 1, 1],
-    #    [1, 0, 0, 0, 0, 0],
-    #    [1, 0, 0, 0, 0, 0],
-    #    [1, 0, 0, 0, 0, 0],
-    #    [1, 0, 0, 0, 0, 0],
-    #    [1, 0, 0, 0, 0, 0],
-    #]
-    #names = ['1', '2', '3', '4', '5', '6']
-    #colors = ['gray', 'lightblue', 'lightblue', 'lightblue', 'lightblue', 'lightblue']
-    ont_query = '''
-        PREFIX owl: <http://www.w3.org/2002/07/owl#>
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX sto: <https://w3id.org/i40/sto#>
-        SELECT ?pred WHERE {
-            sto:IEC_62541 ?pred ?obj .
-        }
-    '''
-    names = fetch_sto_data(ont_query, ['preds'])['preds']
-    names.insert(0, 'sto:IEC_62541')
-    elem_num = len(names) + 1
-    np_mtx = np.zeros((elem_num, elem_num)).astype(int)
-    np_mtx[:, 0] = np.ones(elem_num)
-    #np_mtx[0, :] = np.ones(elem_num)
-    #np_mtx[0, 0] = 0
-    #colors = np.chararray(pred_num, itemsize=5)
-    colors = ['gray'] * elem_num
-    colors[0] = 'lightblue'
-    colors[5] = 'lightgray'
-    colors[6] = 'lightgray'
-    colors[7] = 'lightgray'
-    colors[12] = 'lightgray'
-    colors[17] = 'lightgray'
-    colors[18] = 'lightgray'
-    colors[19] = 'lightgray'
-    colors[22] = 'lightgray'
-    return jsonify({'mtx': np_mtx.tolist(), 'names': names, 'colors': colors})
+    #ont_query = '''
+    #    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    #    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    #    PREFIX sto: <https://w3id.org/i40/sto#>
+    #    SELECT ?pred WHERE {
+    #        sto:IEC_62541 ?pred ?obj .
+    #    }
+    #'''
+    #names = fetch_sto_data(ont_query, ['preds'])['preds']
+    #names.insert(0, 'sto:IEC_62541')
+    #elem_num = len(names) + 1
+    #np_mtx = np.zeros((elem_num, elem_num)).astype(int)
+    #np_mtx[:, 0] = np.ones(elem_num)
+    #colors = ['gray'] * elem_num
+    names = ['sto:IEC_62541', 'rdf:type', 'dcterms:hasLicense', 'sto:hasClassification', 'sto:hasDeveloper', 'dcterms:subject', 'sto:hasDomain', 'sto:hasPublisher', 'sto:relatedTo', 'sto:useStructureOf', 'sto:hasDBpediaResource', 'sto:hasOfficialResource', 'sto:hasTag', 'sto:hasWikipediaArticle', 'dcterms:hasLicense', 'lingg:hypernym', 'rdfs:comment', 'rdfs:label']
+    colors = ['gray', 'lightblue', 'lightgray', 'lightgray', 'lightgray', 'lightgreen', 'lightgray', 'lightgray', 'lightgray', 'lightgreen', 'lightgray', 'lightgray', 'lightgray', 'lightgray', 'lightgray', 'lightgreen', 'lightgray', 'lightgray']
+    mtx = np.zeros((len(names), len(names))).astype(int)
+    mtx[:, 0] = np.ones(len(names))
+
+    return jsonify({'mtx': mtx.tolist(), 'names': names, 'colors': colors})
