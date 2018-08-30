d0bb91bfc517862556ecaa2660f40e6926940eac
mmaltsev <d.kruz@mail.ru>
2018-08-30 12:08:55 +0200

    code cleaned

--- server/blueprints/wheel/wheel.js
+++ server/blueprints/wheel/wheel.js
@@ -167,33 +167,3 @@ function setPrefix(sub) {
     return '_:' + sub
   }
 }
-
-//function dataFetch() {
-//  spinner(isActive = true)
-//  let dataUrl = 'https://gist.githubusercontent.com/mmaltsev/237bf4c09414da02856225428d18010d/raw/76e12ed1baba9cf4870bc1be23cf52226f072312/sto_data.txt'
-//  let namesUrl = 'https://gist.githubusercontent.com/mmaltsev/33dad977c49e672d7cdbddd4b9682cdd/raw/70d97f3b7740d06dc678250cc20b201ec9d7fa01/sto_names.txt'
-//  //d3.json(dataUrl, function(mtx) {
-//  //  d3.json(namesUrl, function(names) {
-//  //    
-//  //  })
-//  //})
-//  getRequest('/wheel/data', (response) => {
-//    spinner(isActive = false)
-//    console.log(response)
-//      // Drawing chart
-//      var data = {
-//        packageNames: response.names,
-//        matrix: response.mtx,
-//        colors: response.colors
-//      }
-//
-//      var chart = d3.chart.dependencyWheel()
-//        .width(800)  // also used for height, since the wheel is in a square
-//        .margin(200)  // used to display package names
-//        .padding(.02)  // separating groups in the wheel
-//
-//      d3.select('#chart')
-//        .datum(data)
-//        .call(chart)
-//  })
-//}
